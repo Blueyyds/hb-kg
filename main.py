9b26cb9a7b55cb64101d20b69f2f68c2a6a91a20
Blueyyds <1412123717@qq.com>
2023-06-15 09:55:20 +0800

    6.15

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from spider import getTopN, extraInfo
+from spider import getTopN, extractInfo, extractBuildingsInfo
 import json
 from copy import deepcopy
+from typing import List, Type
 
 
 # 把爬取到的数据写入文件
@@ -24,32 +25,67 @@ def process(data):
 relations = ["建筑师", "开发商", "结构工程师", "主承包商"]
 
 
+class RelatedEntity:
+    def __init__(self, entity1, entity2, relation):
+        self.entity1 = entity1
+        self.entity2 = entity2
+        self.relation = relation
+
+    def __dict__(self):
+        return {
+            "entity1": self.entity1,
+            "entity2": self.entity2,
+            "relation": self.relation,
+        }
+
+
 # 分析建筑物中含有的关系，并从维基百科中爬取该关系对应实体的信息
 def analyze(buildings):
+    res = []
+    related_entities: List[RelatedEntity] = []
+    for index, building in enumerate(buildings):
+        print(
+            "Processing Relations With %s..., %d / %d"
+            % (building["entity"], index + 1, len(buildings))
+        )
+        entity1 = building["entity"]
+        props = building["props"]
+        for prop, value in props.items():
+            if prop in relations:
+                # 遍历该实体对应关系的所有实体链接
+                links = value["links"]
+                entity = extractInfo(links)
+                for item in entity:
+                    related_entities.append(
+                        RelatedEntity(entity1, item["entity"], prop)
+                    )
+                res.extend(entity)
+
+    related_entities_json = [item.__dict__() for item in related_entities]
+    writeToFile(related_entities_json, "related_entities.json")
+    writeToFile(res, "test3.json")
+
+
+# 获取从建筑物中抽取到的实体链接
+def getLinks(buildings):
     for building in buildings:
         props = building["props"]
-        if "建筑师" in props:
-            set1 = set([])
-            # for architect in props["建筑师"]:
-            #     print(architect)
-            #     architect = architect["text"]
-            #     if architect not in buildings:
-            #         print("Processing %s..." % architect)
-            #         data = extraInfo([(architect, 1)])
-            #         buildings.append(data[0])
+        for prop in props:
+            if "links" in props[prop]:
+                print([item["link"] for item in props[prop]["links"]])
+
+
+# 获取排名前n的建筑物，并从维基百科上爬取信息，最终写入文件
+def work(n):
+    buildings = getTopN(n)
+    data = extractBuildingsInfo(buildings)
+    writeToFile(data, "test.json")
+    writeToFile(process(data), "test2.json")
 
 
 # main函数
 if __name__ == "__main__":
-    # with open("test.json", "r") as file:
-    #     buildings = json.loads(file.read())
-    # analyze(buildings)
-    # data = process(buildings)
-    # writeToFile(data, "test2.json")
-    # for building in buildings:
-    #     print(building["props"]["建筑师"])
     with open("test.json", "r") as file:
         buildings = json.loads(file.read())
-
-    data = process(buildings)
-    writeToFile(data, "test2.json")
+    analyze(buildings)
+    # work(5)
--- spider.py
+++ spider.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import unquote
+import re
 
 
 # 定义一个通用的爬虫函数
-def spider(key):
-    url = "https://zh.wikipedia.org/wiki/" + key
+def spider(url):
     headers = {"Accept-Language": "zh-CN,zh;q=0.9"}
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -41,12 +42,25 @@ def getTopN(n):
     return buildings
 
 
-# 提取表格中的数据
-def extraInfo(keys=[]):
+# 判断参数link是什么类型的链接
+def judgeLink(link):
+    # 如果link是字典，说明是带有文本的链接
+    link = link["link"] if isinstance(link, dict) else link
+    if link.startswith("https://"):  # 1. https链接
+        return 1
+    elif link.startswith("/wiki/"):  # 2. /wiki/开头的链接
+        return 2
+    else:  # 3. 非常规链接
+        return 3
+
+
+# 提取建筑物的信息，属性中包含链接
+def extractBuildingsInfo(keys=[]):
     res = []
     for index, key in enumerate(keys):
+        url = "https://zh.wikipedia.org/wiki/" + key
         print("Processing %s..., %d / %d" % (key, index + 1, len(keys)))
-        info = spider(key).find("table", class_="infobox")
+        info = spider(url).find("table", class_="infobox")
         if info is None:
             continue
         data = {"entity": key, "props": {}}
@@ -55,16 +69,49 @@ def extraInfo(keys=[]):
             cells = row.find_all(["th", "td"])
             if len(cells) == 2:
                 key = cells[0].text.strip()
-                if key == "坐标":
+                if key == "坐标":  # 去除坐标信息
                     continue
-                value = {"text": cells[1].text.strip()}
+                # 去除文本中的锚点链接
+                text = re.sub(r"\[\d+\]", "", cells[1].text.strip())
+                value = {"text": text}
                 if cells[1].findAll("a"):
+                    # 使用unquote函数对链接进行解码
                     links = [
-                        {"text": a.text.strip(), "link": a.get("href")}
+                        {"text": a.text.strip(), "link": unquote((a.get("href")))}
                         for a in cells[1].findAll("a")
+                        if a.text.strip() != ""
+                        and not re.match(r"^\[\d+\]$", a.text.strip())  # 去除类似[1]这样的链接
                     ]
                     value["links"] = links
 
+                data["props"][key] = value
+        res.append(data)
+    return res
+
+
+# 提取表格中的数据
+def extractInfo(links=[]):
+    res = []
+    for index, link in enumerate(links):
+        if judgeLink(link) == 1:  # https链接
+            entity_name = link["text"]
+            url = link["link"]
+        elif judgeLink(link) == 2:  # /wiki/开头的链接
+            entity_name = link["text"]
+            url = "https://zh.wikipedia.org" + link["link"]
+        else: # 非常规链接
+            continue
+        print("Processing %s..., %d / %d" % (entity_name, index + 1, len(links)))
+        info = spider(url).find("table", class_="infobox")
+        if info is None:
+            continue
+        data = {"entity": entity_name, "props": {}}
+        rows = info.find_all("tr")
+        for row in rows:
+            cells = row.find_all(["th", "td"])
+            if len(cells) == 2:
+                key = cells[0].text.strip()  # 属性值
+                value = re.sub(r"\[\d+\]", "", cells[1].text.strip())  # 去除文本中的锚点链接
                 data["props"][key] = value
         res.append(data)
     return res
